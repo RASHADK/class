7e3546f1f85d7bf9530c82469a9e4329e4eecce8
RASHADK <110896723+RASHADK@users.noreply.github.com>
2022-08-29 14:50:04 +0530

    Add files via upload

---
+++ pattern.py
@@ -0,0 +1,14 @@
+# for i in range(0,5):
+#     for j in range(0,5):
+#         print("*")
+#     print("\r")
+# print("///////////////////////")
+for i in range(0,5):
+    for j in range(0,i+1):
+        print("* ",end="")
+    print("\r")
+print(".........................")
+for i in range(0,5):
+    for j in range(0,5):
+        print("* ",end="")
+    print("\n")
